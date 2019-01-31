7f27f10de7d4cfb4e6b23521e5af6108fc70a995
Jeason Lee <ijeasonlee@gmail.com>
2019-01-31 21:41:38 +0800

    Update wechat-tweak.rb

--- wechat-tweak.rb
+++ wechat-tweak.rb
@@ -5,7 +5,7 @@ class WechatTweak < Formula
   desc "WeChatTweak-macOS brew support"
   homepage "https://github.com/JeasonL/homebrew-wechat-tweak"
   url "https://github.com/JeasonL/homebrew-wechat-tweak/archive/1.0.0.zip"
-  sha256 "d18c1985a2c34707b6150768f641f65b18fb9c1089532f3c1efaa01281cdebef"
+  sha256 "f3cca5a67e22e68eac9fe266d8b8c0009fadf4d06d754216f94d1674729828e0"
   
   def install
     bin.install "wechat-tweak"
