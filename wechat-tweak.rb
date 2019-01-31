336967d7ce4df09ec7aa1ac30339b37e95959cbb
Jeason Lee <ijeasonlee@gmail.com>
2019-01-31 23:35:37 +0800

    Update wechat-tweak.rb

--- wechat-tweak.rb
+++ wechat-tweak.rb
@@ -3,8 +3,8 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class WechatTweak < Formula
   desc "WeChatTweak-macOS brew support"
-  homepage "https://github.com/JeasonL/homebrew-wechat-tweak"
-  url "https://github.com/JeasonL/homebrew-wechat-tweak/archive/1.0.0.zip"
+  homepage "https://github.com/WeChatTweak/homebrew-wechat-tweak"
+  url "https://github.com/WeChatTweak/homebrew-wechat-tweak/archive/1.0.0.zip"
   sha256 "f3cca5a67e22e68eac9fe266d8b8c0009fadf4d06d754216f94d1674729828e0"
   
   def install
